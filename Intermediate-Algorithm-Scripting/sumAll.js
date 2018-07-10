19e5ad7708cfe2b0ad95f94937c176a18a1eda2a
ysfscream <894402575@qq.com>
2018-07-10 23:26:34 +0800

    :sparkles: Add practice2018年 7月10日 星期二 23时26分34秒 CST

---
+++ Intermediate-Algorithm-Scripting/sumAll.js
@@ -0,0 +1,15 @@
+// 我们会传递给你一个包含两个数字的数组。返回这两个数字和它们之间所有数字的和。
+function sumAll(arr) {
+  const allNumArr = []
+  arr.sort((prev, next) => {
+    return prev - next
+  })
+  const min = arr[0]
+  const max = arr[1]
+  for(let i=min; i<=max; i+=1) {
+    allNumArr.push(i)
+  }
+  return allNumArr.reduce((prev, next) => prev + next)
+}
+
+console.log(sumAll([4, 1]))
